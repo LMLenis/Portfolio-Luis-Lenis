02b2fecbc627f38cb2bc7fe56a2d8c03ef8a44ae
lmlenis <llenis73@gmail.com>
2023-12-23 19:55:45 -0500

    diez

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -29,7 +29,7 @@ const handleChange = (event) => {
 };
 
   return (
-    <div id= "contact" className=" w-screen 2xl:mt-20   h-screen flex items-center flex-col pt-[5%]  md:pt-[250%] sm:pt-[300%] ">
+    <div id= "contact" className=" w-screen 2xl:mt-20   h-screen flex items-center flex-col pt-[5%] sm:pt-[300%] ">
       <h2 className="font-semibold text-[40px]">Contact</h2>
       <form
         action="https://getform.io/f/2261747c-69b5-456a-b5e8-1d7aed9f024f"
