cda3662cc86922581722a238e5910219e0772139
lmlenis <llenis73@gmail.com>
2024-01-08 12:33:47 -0500

    hoja de vida

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
         My greatest contribution will be in stakeholders management, on-time deliverables and user experience mindset.</p> 
         
         <a
-            href="https://drive.google.com/file/d/1POwramjhkW0OEVB63HKccTH7thwo-hF1/view">
+            href="https://drive.google.com/file/d/1W_kVnZkUiEw5USlSLlg2E0rWBpEiX-Kv/view">
         <button
           type="submit"
           className="rounded border border-slate-800 mt-8 w-[160px] p-2 font-semibold bg-[#42A5F5] text-white text-[20px] duration-300 hover:bg-[#2a6ba0]"
