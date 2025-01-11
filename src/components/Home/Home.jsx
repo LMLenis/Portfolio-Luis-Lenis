30725391f2631cfb51edd4506c0d61c139c29d4e
lmlenis <llenis73@gmail.com>
2025-01-11 14:18:13 -0500

    cambio rol

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
         <div className="md:text-[30px] md:p-10 md:left-[1%] md:w-screen md:pt-[20%] md:static pl-[15%] pt-[8%] w-[1100px] text-black font-normal">
         
         <h1 className="text-[60px] font-bold md:text-[40px] font-serif text-[#42A5F5]" >Luis Lenis</h1> 
-        <p className="text-[40px] font-semibold sm:text-[30px]">Project manager and fullstack developer</p> 
-        <p className="text-[30px] pt-[3%] sm:text-[24px] text-justify">  I am available right now to be part of a company where I can grow 
+        <p className="text-[40px] font-semibold sm:text-[30px]">Project manager</p> 
+        <p className="text-[30px] pt-[3%] sm:text-[24px] text-justify">  Right now I am part of a company where I can grow 
         and at the same time I can contribute with my experience acquired in the world of telecommunications and IT. <br/>
-        My greatest contribution will be in stakeholders management, on-time deliverables and user experience mindset.</p> 
+        My greatest contribution is in process optimization with RPA and AI applications.</p> 
         
         <a
             href="https://drive.google.com/file/d/19aF-fiCkYEL59RhHuchxU-6pkY29X7-4/view">
